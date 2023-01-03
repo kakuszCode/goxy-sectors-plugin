56b6716c6b5f0af59655da73a54c8b0f05bec103
robalmeister <65306693+robalmeister@users.noreply.github.com>
2023-01-03 21:12:09 +0100

    sead

--- src/main/java/pl/robalmeister/goxysectorsplugin/GoxySectorsPlugin.java
+++ src/main/java/pl/robalmeister/goxysectorsplugin/GoxySectorsPlugin.java
@@ -3,15 +3,28 @@ package pl.robalmeister.goxysectorsplugin;
 import eu.okaeri.injector.Injector;
 import eu.okaeri.injector.OkaeriInjector;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.goxy.minecraft.pubsub.PubSub;
+import pl.goxy.minecraft.pubsub.PubSubService;
+import pl.robalmeister.goxysectorsplugin.listener.BlockPlaceListener;
+import pl.robalmeister.goxysectorsplugin.listener.PlayerJoinListener;
+import pl.robalmeister.goxysectorsplugin.listener.PlayerMoveListener;
+
+import java.util.Objects;
 
 public final class GoxySectorsPlugin extends JavaPlugin {
     private BukkitAudiences adventure;
     private Injector injector;
+    private PubSub pubSub;
 
-
+    //redisa instaluje
     @Override
     public void onEnable() {
+        Plugin pubSubPlugin = Objects.requireNonNull(getServer().getPluginManager().getPlugin("goxy-pubsub"), "goxy-pubsub");
+        PubSubService pubSubService = (PubSubService) pubSubPlugin;
+        this.pubSub = pubSubService.getPubSub(this);
+
         injector = OkaeriInjector.create();
         adventure = BukkitAudiences.create(this);
         registerVariablesToInjector();
@@ -27,5 +40,11 @@ public final class GoxySectorsPlugin extends JavaPlugin {
     private void registerVariablesToInjector(){
         injector.registerInjectable(adventure);
         injector.registerInjectable(this);
+        injector.registerInjectable(pubSub);
+    }
+    private void registerListeners(){
+        getServer().getPluginManager().registerEvents(injector.createInstance(PlayerMoveListener.class), this);
+        getServer().getPluginManager().registerEvents(injector.createInstance(PlayerJoinListener.class),this);
+        getServer().getPluginManager().registerEvents(injector.createInstance(BlockPlaceListener.class), this);
     }
 }
--- src/main/java/pl/robalmeister/goxysectorsplugin/listener/PlayerMoveListener.java
+++ src/main/java/pl/robalmeister/goxysectorsplugin/listener/PlayerMoveListener.java
@@ -2,43 +2,82 @@ package pl.robalmeister.goxysectorsplugin.listener;
 
 import eu.okaeri.injector.annotation.Inject;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import pl.goxy.minecraft.api.GoxyApi;
+import pl.goxy.minecraft.api.network.GoxyConnector;
+import pl.goxy.minecraft.api.network.GoxyServer;
+import pl.goxy.minecraft.pubsub.PubSub;
 import pl.robalmeister.goxysectorsplugin.config.Configuration;
 import pl.robalmeister.goxysectorsplugin.data.SectorData;
 import pl.robalmeister.goxysectorsplugin.data.SectorDirectionEnum;
+import pl.robalmeister.goxysectorsplugin.data.user.SectorsUser;
 import pl.robalmeister.goxysectorsplugin.helper.ChatHelper;
+import pl.robalmeister.goxysectorsplugin.pubsub.PubSubAbstract;
+import pl.robalmeister.goxysectorsplugin.serializer.ItemSerializer;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PlayerMoveListener implements Listener {
     @Inject
     private Configuration configuration;
     @Inject
     private BukkitAudiences adventure;
+    @Inject
+    private PubSub pubSub;
+    //test
+
+
+
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
         if (e.getFrom() == e.getTo()) return;
         Location locationTo = e.getTo();
         assert locationTo != null;
-        if (!(locationTo.getX() >= -locationTo.getWorld().getWorldBorder().getSize() - 2 && locationTo.getX() <= locationTo.getWorld().getWorldBorder().getSize() + 2 && locationTo.getZ() >= -locationTo.getWorld().getWorldBorder().getSize() - 2 && locationTo.getZ() <= locationTo.getWorld().getWorldBorder().getSize() + 2)){
+        if (!(locationTo.getX() >= -locationTo.getWorld().getWorldBorder().getSize() - 2 && locationTo.getX() <= locationTo.getWorld().getWorldBorder().getSize() + 2 && locationTo.getZ() >= -locationTo.getWorld().getWorldBorder().getSize() - 2 && locationTo.getZ() <= locationTo.getWorld().getWorldBorder().getSize() + 2)) {
             SectorDirectionEnum direction = getDirection(locationTo);
-            List<SectorData> sectors = configuration.getSectorData().stream().filter(sectorData -> sectorData.getSectorDirectionEnum() == direction).collect(Collectors.toList());
-            if (sectors.isEmpty()){
-                adventure.player(e.getPlayer()).sendMessage(ChatHelper.parse("pierdol sie kurwy"));
-                return;
-            }
+           configuration.getSectorData().stream().filter(sectorData -> sectorData.getSectorDirectionEnum() == direction).forEach(sectorData -> {
+               GoxyServer server = GoxyApi.getNetworkManager().getServer(sectorData.getUuidServer());
+               pubSub.sendServer("getSectorUser", new SectorsUser(p.getUniqueId(),
+                       p.getName(),
+                       ItemSerializer.serialize(p.getInventory().getContents()),
+                       ItemSerializer.serialize(p.getInventory().getArmorContents()),
+                       p.getGameMode(),
+                       p.getFoodLevel(),
+                       p.getHealth(),
+                       p.getLevel(),
+                       p.getExp(),
+                       p.isFlying()),server).thenAccept(unused -> {
+                           p.getInventory().setContents(null);
+                           p.getInventory().setArmorContents(null);
+                           adventure.player(p).sendMessage(ChatHelper.parse("nawiazuje polaczenie z twoim starym"));
+                           server.connect(GoxyApi.getPlayerStorage().getPlayer(p.getUniqueId())).thenAccept(connectResult -> {
+                                if (connectResult == GoxyConnector.ConnectResult.FAIL){
+                                    adventure.player(p).sendMessage(ChatHelper.parse("kurwa nie dziala co jest"));
+                                }
+                           });
+               });
+
+           });
+
         }
 
     }
 
 
-    public SectorDirectionEnum getDirection(Location loc){
-
-
+    public SectorDirectionEnum getDirection(Location loc) {
+        Validate.notNull(loc, "Location cannot be null");
+        if (loc.getX() >= -loc.getWorld().getWorldBorder().getSize() - 2 ) return SectorDirectionEnum.WEST;
+        if (loc.getX() >= loc.getWorld().getWorldBorder().getSize() - 2 ) return SectorDirectionEnum.EAST;
+        if (loc.getZ() >= loc.getWorld().getWorldBorder().getSize() - 2 ) return SectorDirectionEnum.SOUTH;
+        if (loc.getZ() >= -loc.getWorld().getWorldBorder().getSize() - 2 ) return SectorDirectionEnum.NORTH;
         return SectorDirectionEnum.DEFAULT;
     }
+
+
 }
